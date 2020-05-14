e1160845f68e01dfb8b058536ed27d2095ac1b98
Andrew Bettke <andrew.bettke@ns8.com>
2020-05-14 12:57:19 -0400

    Add better list styling.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,24 +12,37 @@
 </template>
 
 <script>
-export default {
-  name: 'TodoList',
-  props: {
-    todos: {
-      type: Array,
-      default: () => [],
-    }
-  },
-  methods: {
-    toggleTodo: function (index) {
-      this.$parent.toggleTodo(index);
+  export default {
+    name: 'TodoList',
+    props: {
+      todos: {
+        type: Array,
+        default: () => [],
+      }
+    },
+    methods: {
+      toggleTodo: function (index) {
+        this.$parent.toggleTodo(index);
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.completed {
-  text-decoration: line-through;
-}
+  ul {
+    padding: 0;
+    list-style-type: none;
+  }
+
+  li {
+    cursor: pointer;
+  }
+
+  li:hover {
+    background-color: rgba(0, 0, 0, 0.25);
+  }
+
+  .completed {
+    text-decoration: line-through;
+  }
 </style>
