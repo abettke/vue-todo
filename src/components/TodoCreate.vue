c242acdc407298d890777dd118f7166fbd853c01
Andrew Bettke <andrew.bettke@ns8.com>
2020-05-17 17:18:30 -0400

    Refactor form to use vuetify components.

--- src/components/TodoCreate.vue
+++ src/components/TodoCreate.vue
@@ -1,8 +1,15 @@
 <template>
-  <form v-on:submit="onSubmit" ref="todoCreateForm">
-    <input v-model="text" placeholder="Add new to-do..."/>
-    <div v-on:click="onSubmit">+</div>
-  </form>
+  <v-form v-on:submit="onSubmit" ref="todoCreateForm">
+    <v-text-field
+      v-model="text"
+      label="Add To-Do"
+      required
+    >
+    </v-text-field>
+    <v-btn icon type="submit">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
@@ -11,15 +18,16 @@
   export default {
     name: 'TodoCreate',
     data: () => ({
+      valid: false,
       text: '',
     }),
     methods: {
-      ...mapActions(['addToDo']),
       onSubmit: function(ev) {
         ev.preventDefault();
         this.addToDo(this.text);
         this.$refs.todoCreateForm.reset();
-      }
+      },
+      ...mapActions(['addToDo']),
     }
   }
 </script>
@@ -27,24 +35,6 @@
 <style scoped>
   form {
     display: flex;
-  }
-
-  input{
-    flex: 1 0;
-    font-size: 16px;
-    border: none;
-    border-bottom: .5px solid rgba(0, 0, 0, 0.5);
-  }
-
-  div {
-    border: 1px solid rgba(0, 0, 0, 0.5);
-    border-radius: 4px;
-    cursor: pointer;
-    margin: 0 8px;
-    width: 22px;
-  }
-
-  div:hover {
-    background: rgba(0, 0, 0, .1);
+    align-items: center;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store'
+import { vuetify } from './plugins';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   store,
+  vuetify,
 }).$mount('#app')
